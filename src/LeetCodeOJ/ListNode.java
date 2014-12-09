55edbf53355aae73e234d1d28b84d1d988e7fcac
=idiotshi <=hm0214@126.com>
2014-12-10 00:02:40 +0800

    add merge two sorted list node

--- src/LeetCodeOJ/ListNode.java
+++ src/LeetCodeOJ/ListNode.java
@@ -2,6 +2,10 @@ package LeetCodeOJ;
 
 public class ListNode {
 	Object value;
+	/*
+	 * different problem has different ListNode definition
+	 */
+	int val;
 	ListNode next;
 
 	
@@ -59,4 +63,41 @@ public class ListNode {
 		
 		return head;
 	}
+	
+	
+	/*
+	 * 2014/12/10
+	 */
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l2 == null) return l1;
+        if(l1 == null) return l2;
+        
+        ListNode head = null;
+        if(l1.val < l2.val){
+        	head = l1;
+        	l1 = l1.next;
+        }else{
+        	head = l2;
+        	l2 = l2.next;
+        }
+        
+        ListNode preNode = head;
+        while(l1 != null && l2 != null){
+        	if(l1.val < l2.val){
+        		preNode.next = l1;
+        		l1 = l1.next;
+        	}else{
+        		preNode.next = l2;
+        		l2 = l2.next;
+        	}
+        	preNode = preNode.next;
+        }
+        
+        if(l1 == null)
+        	preNode.next = l2;
+        else
+        	preNode.next = l1;
+        
+        return head;  
+    }
 }
